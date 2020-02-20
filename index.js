c67ea9dd196a91e1ce05ba2c540c3d07fdcc831c
Owner-Aube <58786143+Owner-Aube@users.noreply.github.com>
2020-02-20 15:22:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const token = process.env.token;
 client.commands = new Discord.Collection()
 const fs = require('fs');
 
