505c7a4d3087085ac0b3a3fc14c2fee35495a0f6
Owner-Aube <58786143+Owner-Aube@users.noreply.github.com>
2020-02-20 15:09:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.commands = new Discord.Collection()
+const fs = require('fs');
+
+
+fs.readdir('./Commandes/', (error, f) => {
+    if (error) { return console.error(error); }
+        let commandes = f.filter(f => f.split('.').pop() === 'js');
+        if (commandes.length <= 0) { return console.log('Aucune commande trouvée !'); }
+
+        commandes.forEach((f) => {
+            let commande = require(`./Commandes/${f}`);
+            console.log(`${f} -> Ready`);
+            client.commands.set(commande.help.name, commande);
+            
+        });
+});
+
+fs.readdir('./Events/', (error, f) => {
+    if (error) { return console.error(error); }
+        console.log(`${f.length} events chargés`);
+
+        f.forEach((f) => {
+            let events = require(`./Events/${f}`);
+            let event = f.split('.')[0];
+            client.on(event, events.bind(null, client));
+        });
+        
+});
+
+client.login('Njc5ODA0MTc3MTM1OTYwMDY0.Xk5kHA.m8oBai0WNt_0eoROreCBXXUuQh0');
