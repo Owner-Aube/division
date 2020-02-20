49ee7439a7d5bb8cbd790db3bf532311b9453917
Owner-Aube <58786143+Owner-Aube@users.noreply.github.com>
2020-02-20 15:30:00 +0100

    Update nitrochecker.js

--- Commandes/nitrochecker.js
+++ Commandes/nitrochecker.js
@@ -1,17 +1,1 @@
-const Discord = require('discord.js');
-
-module.exports.run = (client, message, args) => {
-
-    let discordnitrocheckerdivision = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setTitle("**__Division Nitro Checker__**")
-    .addField("**__Verification de votre liste ...__**", ':x: Aucun Nitro Valable à été trouvé.')
-    .setFooter("Division -> https://discord.gg/w7JBMMh");
-
-    message.channel.send(discordnitrocheckerdivision);
-
-    }
-    
-    module.exports.help = {
-        name: 'nitrochecker'
-    };
+Code invalide.
